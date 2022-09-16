0a8333ac2196e4f82864c1f4fdaa6a3ee329bc8e
Kaja <kathleengracenoble@gmail.com>
2022-09-16 13:59:57 -0600

    added init_db.py and /books in app.py

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask 
+from flask import Flask, request
+import psycopg2
+import os
+import json 
 
 app = Flask(__name__)
+
+
+
+# connect to database
+def db_connect(autocommit:bool = False):
+    # params = config(db)
+    conn = psycopg2.connect(
+        host="postgres://ergtrack_user:Np4qF4P1vCFWwNDrdF2Qp3xOwtmD7s4Y@dpg-cchp06irrk0c3kinmukg-a/ergtrack",
+        database="ergtrack",
+        user=os.environ['DB_USERNAME'],
+        password=os.environ['DB_PASSWORD'])
+    cur = conn.cursor()
+    conn.autocommit = autocommit
+    return conn, cur
 
 @app.route('/')
 def simple_fn():
     return 'hello world!'
+
+@app.route('/books')
+def db():
+    page = request.args['page']
+    conn, cur = db_connect()
+    cur.execute("SELECT * FROM books WHERE page=%s", (f'{page}',))
+    book_info = cur.fetchall()
+    cur.close()
+    conn.close()
+    return json.dumps({'body':book_info})
