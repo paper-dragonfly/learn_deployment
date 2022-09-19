6e467cca50e02e5d7b2a21a87f54dee42dbe7b3c
Kaja <kathleengracenoble@gmail.com>
2022-09-19 16:10:11 -0600

    populated .env changed app.py accordingly

--- flask/app.py
+++ flask/app.py
@@ -2,13 +2,24 @@ from flask import Flask, request
 import psycopg2
 import os
 import json 
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+load_dotenv()
+
+def get_connection_str():
+    env = os.getenv('ENVIRONMENT')   
+    if env == 'production':
+        conn_str = os.getenv('INTERNAL_CONNECTION_STR') 
+    elif env == 'dev':
+        conn_str = os.getenv('EXTERNAL_CONNECTION_STR') 
+    return conn_str 
+
+CONN_STR = get_connection_str()
 
 # connect to database
 def db_connect(autocommit:bool = False):
-    # params = config(db)
-    conn = psycopg2.connect('postgres://ergtrack_user:Np4qF4P1vCFWwNDrdF2Qp3xOwtmD7s4Y@dpg-cchp06irrk0c3kinmukg-a/ergtrack')
+    conn = psycopg2.connect(CONN_STR)
     cur = conn.cursor()
     conn.autocommit = autocommit
     return conn, cur
@@ -19,7 +30,6 @@ def simple_fn():
 
 @app.route('/books')
 def db():
-    # page = request.args['page']
     conn, cur = db_connect()
     cur.execute("SELECT * FROM books")
     book_info = cur.fetchall()
